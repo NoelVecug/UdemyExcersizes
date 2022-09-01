d2f2fab1d1227c35b1dce3af28b09dfb50ebd57b
NoelVecug <geesegagaga@mail.ru>
2022-09-01 21:01:18 +0300

    exercise 15(lesson 70)

---
+++ src/NumberPalindrome.java
@@ -0,0 +1,19 @@
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number){
+        int reverse = 0;
+        int num = number;
+            while (num > 0){
+                int lastDigit = num % 10;
+                reverse *= 10;
+                reverse += lastDigit;
+                num /= 10;
+            }
+        while (num < 0){
+            int lastDigit = num % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            num /= 10;
+        }
+            return reverse == number;
+    }
+}
